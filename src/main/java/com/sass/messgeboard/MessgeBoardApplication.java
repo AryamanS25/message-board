bcd052e8905d5915b2cb5ed99f67d82e9ecbed28
AryamanS25 <109213785+AryamanS25@users.noreply.github.com>
2022-08-05 16:57:27 +0530

    Initial commit

---
+++ src/main/java/com/sass/messgeboard/MessgeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.sass.messgeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessgeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessgeBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sass/messgeboard/MessgeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sass.messgeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessgeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
